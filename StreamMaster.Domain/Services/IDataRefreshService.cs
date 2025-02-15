c040d6b99b1c53cdbfd244817e58f52de4149a46
Carl Reid <kontakt@carlreid.dk>
2025-02-15 10:34:04 +0100

    refactor: Consolidate `IDataRefreshService`

--- StreamMaster.Domain/Services/IDataRefreshService.cs
+++ StreamMaster.Domain/Services/IDataRefreshService.cs
@@ -1,30 +1,91 @@
 namespace StreamMaster.Domain.Services
 {
-    public interface IDataRefreshService: IDataRefreshServicePartial
+    public interface IDataRefreshService
     {
         Task SetField(List<FieldData> fieldData);
+
         Task ClearByTag(string Entity, string Tag);
+
         Task RefreshAll();
+
         Task Refresh(string command);
+
         Task RefreshDownloadServiceStatus();
+
         Task RefreshVs(bool alwaysRun = false);
+
+        Task RefreshUserGroups(bool alwaysRun = false);
+
         Task RefreshStreamGroups(bool alwaysRun = false);
+
         Task RefreshStreamGroupSMChannelLinks(bool alwaysRun = false);
+
         Task RefreshStatistics(bool alwaysRun = false);
+
         Task RefreshSMTasks(bool alwaysRun = false);
+
         Task RefreshSMStreams(bool alwaysRun = false);
+
         Task RefreshSMChannels(bool alwaysRun = false);
+
         Task RefreshSMChannelStreamLinks(bool alwaysRun = false);
+
         Task RefreshSMChannelChannelLinks(bool alwaysRun = false);
+
         Task RefreshSettings(bool alwaysRun = false);
+
         Task RefreshSchedulesDirect(bool alwaysRun = false);
+
         Task RefreshProfiles(bool alwaysRun = false);
+
+        Task RefreshM3UGroups(bool alwaysRun = false);
+
         Task RefreshM3UFiles(bool alwaysRun = false);
+
         Task RefreshLogs(bool alwaysRun = false);
+
         Task RefreshLogos(bool alwaysRun = false);
-        Task RefreshEPG(bool alwaysRun = false);
+
+        Task RefreshEPGs(bool alwaysRun = false);
+
         Task RefreshEPGFiles(bool alwaysRun = false);
+
         Task RefreshCustom(bool alwaysRun = false);
+
         Task RefreshChannelGroups(bool alwaysRun = false);
+
+        Task RefreshSDReady();
+
+        Task RefreshAllGroups();
+
+        Task AuthLogOut();
+
+        Task SendStatus(ImageDownloadServiceStatus imageDownloadServiceStatus);
+
+        Task RefreshEPGColors();
+
+        Task RefreshSelectedStationIds();
+
+        Task RefreshStationPreviews();
+
+        Task RefreshOutputProfiles();
+
+        Task RefreshCommandProfiles();
+
+        Task IsSystemReady();
+
+        Task TaskIsRunning();
+
+        Task SendMessage(SMMessage smMessage);
+
+        Task SendSMTasks(List<SMTask> smTasks);
+
+        Task RefreshAllEPG();
+
+        Task RefreshAllM3U();
+
+        Task RefreshAllSMChannels();
+
+        Task RefreshVideoInfos();
     }
 }
--- StreamMaster.Infrastructure/Services/DataRefreshService.cs
+++ StreamMaster.Infrastructure/Services/DataRefreshService.cs
@@ -2,247 +2,420 @@ using Microsoft.AspNetCore.SignalR;
 
 using StreamMaster.Application.Interfaces;
 using StreamMaster.Application.Hubs;
-using StreamMaster.Application.Services;
 using StreamMaster.Domain.Configuration;
+using StreamMaster.Application.SMStreams.Commands;
 
 namespace StreamMaster.Infrastructure.Services;
 
-public partial class DataRefreshService(IHubContext<StreamMasterHub, IStreamMasterHub> hub) : IDataRefreshService, IDataRefreshServicePartial
+public partial class DataRefreshService(IHubContext<StreamMasterHub, IStreamMasterHub> hub) : IDataRefreshService
 {
     public async Task RefreshAll()
     {
-        await RefreshChannelGroups(true);
-        await RefreshCustom(true);
-        await RefreshEPG(true);
-        await RefreshEPGFiles(true);
-        await RefreshLogos(true);
-        await RefreshLogs(true);
-        await RefreshM3UFiles(true);
-        await RefreshProfiles(true);
-        await RefreshSchedulesDirect(true);
-        await RefreshSettings(true);
-        await RefreshSMChannelChannelLinks(true);
-        await RefreshSMChannels(true);
-        await RefreshSMChannelStreamLinks(true);
-        await RefreshSMStreams(true);
-        await RefreshSMTasks(true);
-        await RefreshStatistics(true);
-        await RefreshStreamGroups(true);
-        await RefreshStreamGroupSMChannelLinks(true);
-        await RefreshVs(true);
+        await RefreshChannelGroups(alwaysRun: true);
+        await RefreshCustom(alwaysRun: true);
+        await RefreshEPGFiles(alwaysRun: true);
+        await RefreshEPGs(alwaysRun: true);
+        await RefreshLogos(alwaysRun: true);
+        await RefreshLogs(alwaysRun: true);
+        await RefreshM3UFiles(alwaysRun: true);
+        await RefreshM3UGroups(alwaysRun: true);
+        await RefreshProfiles(alwaysRun: true);
+        await RefreshSchedulesDirect(alwaysRun: true);
+        await RefreshSettings(alwaysRun: true);
+        await RefreshSMChannelChannelLinks(alwaysRun: true);
+        await RefreshSMChannels(alwaysRun: true);
+        await RefreshSMChannelStreamLinks(alwaysRun: true);
+        await RefreshSMStreams(alwaysRun: true);
+        await RefreshSMTasks(alwaysRun: true);
+        await RefreshStatistics(alwaysRun: true);
+        await RefreshStreamGroups(alwaysRun: true);
+        await RefreshStreamGroupSMChannelLinks(alwaysRun: true);
+        await RefreshUserGroups(alwaysRun: true);
+        await RefreshVs(alwaysRun: true);
     }
 
     public async Task RefreshChannelGroups(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetChannelGroups");
+            await hub.Clients.All.DataRefresh("GetChannelGroupsFromSMChannels");
+            await hub.Clients.All.DataRefresh("GetPagedChannelGroups");
         }
-
-        await hub.Clients.All.DataRefresh("GetChannelGroups");
-        await hub.Clients.All.DataRefresh("GetChannelGroupsFromSMChannels");
-        await hub.Clients.All.DataRefresh("GetPagedChannelGroups");
     }
 
     public async Task RefreshCustom(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetCustomPlayLists");
+            await hub.Clients.All.DataRefresh("GetIntroPlayLists");
         }
-
-        await hub.Clients.All.DataRefresh("GetCustomPlayLists");
-        await hub.Clients.All.DataRefresh("GetIntroPlayLists");
-    }
-
-    public async Task RefreshEPG(bool alwaysRun = false)
-    {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
-        {
-            return;
-        }
-
-        await hub.Clients.All.DataRefresh("GetEPGColors");
     }
 
     public async Task RefreshEPGFiles(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetEPGFileNames");
+            await hub.Clients.All.DataRefresh("GetEPGFiles");
+            await hub.Clients.All.DataRefresh("GetEPGNextEPGNumber");
+            await hub.Clients.All.DataRefresh("GetPagedEPGFiles");
         }
+    }
 
-        await hub.Clients.All.DataRefresh("GetEPGFileNames");
-        await hub.Clients.All.DataRefresh("GetEPGFiles");
-        await hub.Clients.All.DataRefresh("GetEPGNextEPGNumber");
-        await hub.Clients.All.DataRefresh("GetPagedEPGFiles");
+    public async Task RefreshEPGs(bool alwaysRun = false)
+    {
+        if (alwaysRun || BuildInfo.IsSystemReady)
+        {
+            await hub.Clients.All.DataRefresh("GetEPGColors");
+        }
     }
 
     public async Task RefreshLogos(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetCustomLogos");
+            await hub.Clients.All.DataRefresh("GetLogos");
         }
-
-        await hub.Clients.All.DataRefresh("GetCustomLogos");
-        await hub.Clients.All.DataRefresh("GetLogos");
     }
 
     public async Task RefreshLogs(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetLogNames");
         }
-
-        await hub.Clients.All.DataRefresh("GetLogNames");
     }
 
     public async Task RefreshM3UFiles(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetM3UFileNames");
+            await hub.Clients.All.DataRefresh("GetM3UFiles");
+            await hub.Clients.All.DataRefresh("GetPagedM3UFiles");
         }
+    }
 
-        await hub.Clients.All.DataRefresh("GetM3UFileNames");
-        await hub.Clients.All.DataRefresh("GetM3UFiles");
-        await hub.Clients.All.DataRefresh("GetPagedM3UFiles");
+    public async Task RefreshM3UGroups(bool alwaysRun = false)
+    {
+        if (alwaysRun || BuildInfo.IsSystemReady)
+        {
+            await hub.Clients.All.DataRefresh("GetM3UGroups");
+            await hub.Clients.All.DataRefresh("GetPagedM3UGroups");
+        }
     }
 
     public async Task RefreshProfiles(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetCommandProfiles");
+            await hub.Clients.All.DataRefresh("GetOutputProfiles");
         }
-
-        await hub.Clients.All.DataRefresh("GetCommandProfiles");
-        await hub.Clients.All.DataRefresh("GetOutputProfiles");
     }
 
     public async Task RefreshSchedulesDirect(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetAvailableCountries");
+            await hub.Clients.All.DataRefresh("GetHeadendsToView");
+            await hub.Clients.All.DataRefresh("GetSDReady");
+            await hub.Clients.All.DataRefresh("GetSelectedStationIds");
+            await hub.Clients.All.DataRefresh("GetStationChannelNames");
+            await hub.Clients.All.DataRefresh("GetStationPreviews");
+            await hub.Clients.All.DataRefresh("GetSubScribedHeadends");
+            await hub.Clients.All.DataRefresh("GetSubscribedLineups");
         }
-
-        await hub.Clients.All.DataRefresh("GetAvailableCountries");
-        await hub.Clients.All.DataRefresh("GetHeadendsToView");
-        await hub.Clients.All.DataRefresh("GetSelectedStationIds");
-        await hub.Clients.All.DataRefresh("GetStationChannelNames");
-        await hub.Clients.All.DataRefresh("GetStationPreviews");
-        await hub.Clients.All.DataRefresh("GetSubScribedHeadends");
-        await hub.Clients.All.DataRefresh("GetSubscribedLineups");
     }
 
     public async Task RefreshSettings(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetSettings");
         }
-
-        await hub.Clients.All.DataRefresh("GetSettings");
     }
 
     public async Task RefreshSMChannelChannelLinks(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetSMChannelChannels");
         }
-
-        await hub.Clients.All.DataRefresh("GetSMChannelChannels");
     }
 
     public async Task RefreshSMChannels(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetPagedSMChannels");
         }
-
-        await hub.Clients.All.DataRefresh("GetPagedSMChannels");
     }
 
     public async Task RefreshSMChannelStreamLinks(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetSMChannelStreams");
         }
-
-        await hub.Clients.All.DataRefresh("GetSMChannelStreams");
     }
 
     public async Task RefreshSMStreams(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetPagedSMStreams");
         }
-
-        await hub.Clients.All.DataRefresh("GetPagedSMStreams");
     }
 
     public async Task RefreshSMTasks(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetSMTasks");
         }
-
-        await hub.Clients.All.DataRefresh("GetSMTasks");
     }
 
     public async Task RefreshStatistics(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetChannelMetrics");
+            await hub.Clients.All.DataRefresh("GetDownloadServiceStatus");
+            await hub.Clients.All.DataRefresh("GetIsSystemReady");
+            await hub.Clients.All.DataRefresh("GetSystemStatus");
+            await hub.Clients.All.DataRefresh("GetTaskIsRunning");
+            await hub.Clients.All.DataRefresh("GetVideoInfos");
         }
-
-        await hub.Clients.All.DataRefresh("GetChannelMetrics");
-        await hub.Clients.All.DataRefresh("GetDownloadServiceStatus");
-        await hub.Clients.All.DataRefresh("GetIsSystemReady");
-        await hub.Clients.All.DataRefresh("GetStreamConnectionMetricDatas");
-        await hub.Clients.All.DataRefresh("GetSystemStatus");
-        await hub.Clients.All.DataRefresh("GetTaskIsRunning");
-        await hub.Clients.All.DataRefresh("GetVideoInfos");
     }
 
     public async Task RefreshStreamGroups(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetPagedStreamGroups");
+            await hub.Clients.All.DataRefresh("GetStreamGroupProfiles");
+            await hub.Clients.All.DataRefresh("GetStreamGroups");
         }
-
-        await hub.Clients.All.DataRefresh("GetPagedStreamGroups");
-        await hub.Clients.All.DataRefresh("GetStreamGroupProfiles");
-        await hub.Clients.All.DataRefresh("GetStreamGroups");
     }
 
     public async Task RefreshStreamGroupSMChannelLinks(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
         {
-            return;
+            await hub.Clients.All.DataRefresh("GetStreamGroupSMChannels");
         }
+    }
 
-        await hub.Clients.All.DataRefresh("GetStreamGroupSMChannels");
+    public async Task RefreshUserGroups(bool alwaysRun = false)
+    {
+        if (alwaysRun || BuildInfo.IsSystemReady)
+        {
+            await hub.Clients.All.DataRefresh("GetPagedUserGroups");
+            await hub.Clients.All.DataRefresh("GetUserGroups");
+        }
     }
 
     public async Task RefreshVs(bool alwaysRun = false)
     {
-        if (!alwaysRun && !BuildInfo.IsSystemReady)
+        if (alwaysRun || BuildInfo.IsSystemReady)
+        {
+            await hub.Clients.All.DataRefresh("GetVs");
+        }
+    }
+
+    public async Task AuthLogOut()
+    {
+        await hub.Clients.All.AuthLogOut();
+    }
+
+    public async Task IsSystemReady()
+    {
+        await hub.Clients.All.IsSystemReady(BuildInfo.IsSystemReady);
+    }
+
+    public async Task TaskIsRunning()
+    {
+        await hub.Clients.All.TaskIsRunning(BuildInfo.IsTaskRunning);
+    }
+
+    public async Task RefreshVideoInfos()
+    {
+        if (!BuildInfo.IsSystemReady)
         {
             return;
         }
 
-        await hub.Clients.All.DataRefresh("GetVs");
+        await hub.Clients.All.DataRefresh("GetVideoInfos");
+    }
+
+    public async Task RefreshOutputProfiles()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await hub.Clients.All.DataRefresh("GetOutputProfiles");
+    }
+
+    public async Task RefreshCommandProfiles()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await hub.Clients.All.DataRefresh("GetCommandProfiles");
+    }
+
+    public async Task RefreshDownloadServiceStatus()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await hub.Clients.All.DataRefresh("GetDownloadServiceStatus");
+    }
+
+    public async Task SendStatus(ImageDownloadServiceStatus imageDownloadServiceStatus)
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await hub.Clients.All.SendStatus(imageDownloadServiceStatus);
+    }
+
+    public async Task RefreshAllGroups()
+    {
+        if (BuildInfo.IsSystemReady)
+        {
+            await RefreshM3UGroups();
+            await RefreshUserGroups();
+        }
+    }
+
+    public async Task RefreshStationPreviews()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await hub.Clients.All.DataRefresh("GetStationPreviews");
+    }
+
+    public async Task RefreshSelectedStationIds()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await hub.Clients.All.DataRefresh("GetSelectedStationIds");
+    }
+
+    public async Task Refresh(string command)
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+
+        await hub.Clients.All.DataRefresh(command);
+    }
+
+    public async Task RefreshAllEPG()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await RefreshEPGFiles();
+        await RefreshEPGs();
+        await hub.Clients.All.DataRefresh("GetStationChannelNames");
+    }
+
+    public async Task RefreshAllM3U()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await RefreshM3UFiles();
+        await RefreshSMStreams();
+        await RefreshSMChannels();
+        await RefreshSMChannelStreamLinks();
+        await RefreshChannelGroups();
+    }
+
+    public async Task RefreshSDReady()
+    {
+        if (BuildInfo.IsSystemReady)
+        {
+            await hub.Clients.All.DataRefresh("GetSDReady");
+        }
+    }
+
+    public async Task RefreshAllSMChannels()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await RefreshSMChannels();
+        await RefreshSMChannelStreamLinks();
+        await RefreshStreamGroupSMChannelLinks();
+        await RefreshChannelGroups();
+    }
+
+    public async Task ClearByTag(string Entity, string Tag)
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+
+        await hub.Clients.All.ClearByTag(new ClearByTag(Entity, Tag));
+    }
+
+    public async Task RefreshEPGColors()
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+        await hub.Clients.All.DataRefresh("GetEPGColors");
+    }
+
+    public async Task SetField(List<FieldData> fieldData)
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+
+        await hub.Clients.All.SetField(fieldData);
+    }
+
+    public async Task SendSMTasks(List<SMTask> smTasks)
+    {
+        //if (!BuildInfo.IsSystemReady)
+        //{
+        //    return;
+        //}
+
+        await hub.Clients.All.SendSMTasks(smTasks);
+    }
+
+    public async Task SendMessage(SMMessage smMessage)
+    {
+        if (!BuildInfo.IsSystemReady)
+        {
+            return;
+        }
+
+        await hub.Clients.All.SendMessage(smMessage);
     }
 }
