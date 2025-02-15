0b37c59cf3a38707452246ea57ca49e469f80cf7
Carl Reid <kontakt@carlreid.dk>
2025-02-15 14:08:29 +0100

    style: Auto formatting

--- StreamMaster.WebUI/components/buttons/SimpleButton.tsx
+++ StreamMaster.WebUI/components/buttons/SimpleButton.tsx
@@ -1,24 +1,24 @@
-import SMButton from '@components/sm/SMButton';
-import { useIsTrue } from '@lib/redux/hooks/isTrue';
+import SMButton from "@components/sm/SMButton";
+import { useIsTrue } from "@lib/redux/hooks/isTrue";
 
 interface SMSimpleButtonProperties {
-  dataKey: string;
+	dataKey: string;
 }
 
 const SimpleButton = ({ dataKey }: SMSimpleButtonProperties) => {
-  const { isTrue, setIsTrue } = useIsTrue(dataKey);
+	const { isTrue, setIsTrue } = useIsTrue(dataKey);
 
-  return (
-    <SMButton
-      icon="pi-window-maximize"
-      iconFilled
-      buttonClassName="sm-simple-button icon-blue"
-      onClick={() => {
-        setIsTrue(!isTrue);
-      }}
-      tooltip={isTrue ? 'Expand' : 'Shrink'}
-    />
-  );
+	return (
+		<SMButton
+			icon="pi-window-maximize"
+			iconFilled
+			buttonClassName="sm-simple-button icon-blue"
+			onClick={() => {
+				setIsTrue(!isTrue);
+			}}
+			tooltip={isTrue ? "Expand" : "Shrink"}
+		/>
+	);
 };
 
 export default SimpleButton;
