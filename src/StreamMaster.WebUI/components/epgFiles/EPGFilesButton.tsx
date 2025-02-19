46d04388b68de4e0aa639bd39970f111afafadfa
Carl Reid <kontakt@carlreid.dk>
2025-02-19 16:35:38 +0100

    fix: Use pencil icon to indicate editing

--- src/StreamMaster.WebUI/components/epgFiles/EPGFilesButton.tsx
+++ src/StreamMaster.WebUI/components/epgFiles/EPGFilesButton.tsx
@@ -1,33 +1,33 @@
-import SMPopUp, { SMPopUpRef } from '@components/sm/SMPopUp';
-import { useIsTrue } from '@lib/redux/hooks/isTrue';
-import { memo, useRef } from 'react';
-import EPGFileCreateDialog from './EPGFileCreateDialog';
-import EPGFilesDataSelector from './EPGFilesDataSelector';
+import SMPopUp, { SMPopUpRef } from "@components/sm/SMPopUp";
+import { useIsTrue } from "@lib/redux/hooks/isTrue";
+import { memo, useRef } from "react";
+import EPGFileCreateDialog from "./EPGFileCreateDialog";
+import EPGFilesDataSelector from "./EPGFilesDataSelector";
 
 const EPGFilesButton = () => {
-  const ref = useRef<SMPopUpRef>(null);
-  const closeOverlay = () => ref.current?.hide();
-  const { isTrue: smTableIsSimple } = useIsTrue('isSimple');
-  return (
-    <SMPopUp
-      buttonClassName="sm-w-4rem icon-green"
-      buttonLabel="EPG"
-      contentWidthSize="5"
-      modal
-      header={<EPGFileCreateDialog onUploadComplete={closeOverlay} />}
-      icon="pi-upload"
-      iconFilled
-      info=""
-      placement={smTableIsSimple ? 'bottom-end' : 'bottom'}
-      ref={ref}
-      title="EPG FILES"
-    >
-      <EPGFilesDataSelector />
-    </SMPopUp>
-  );
+	const ref = useRef<SMPopUpRef>(null);
+	const closeOverlay = () => ref.current?.hide();
+	const { isTrue: smTableIsSimple } = useIsTrue("isSimple");
+	return (
+		<SMPopUp
+			buttonClassName="sm-w-6rem icon-green"
+			buttonLabel="EPG Files"
+			contentWidthSize="5"
+			modal
+			header={<EPGFileCreateDialog onUploadComplete={closeOverlay} />}
+			icon="pi-pencil"
+			iconFilled
+			info=""
+			placement={smTableIsSimple ? "bottom-end" : "bottom"}
+			ref={ref}
+			title="EPG FILES"
+		>
+			<EPGFilesDataSelector />
+		</SMPopUp>
+	);
 };
 
-EPGFilesButton.displayName = 'EPGFilesButton';
+EPGFilesButton.displayName = "EPGFilesButton";
 
 export interface M3UFilesEditorProperties {}
 
