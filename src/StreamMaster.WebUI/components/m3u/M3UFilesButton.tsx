5c9f5247e8886cbf72f4167f2c49e5201a15e8de
Carl Reid <kontakt@carlreid.dk>
2025-02-19 16:35:48 +0100

    fix: Extend button name

--- src/StreamMaster.WebUI/components/m3u/M3UFilesButton.tsx
+++ src/StreamMaster.WebUI/components/m3u/M3UFilesButton.tsx
@@ -1,33 +1,33 @@
-import SMPopUp, { SMPopUpRef } from '@components/sm/SMPopUp';
-import { memo, useRef } from 'react';
-import M3UFileCreateDialog from './M3UFileCreateDialog';
-import M3UFilesDataSelector from './M3UFilesDataSelector';
+import SMPopUp, { SMPopUpRef } from "@components/sm/SMPopUp";
+import { memo, useRef } from "react";
+import M3UFileCreateDialog from "./M3UFileCreateDialog";
+import M3UFilesDataSelector from "./M3UFilesDataSelector";
 
 const M3UFilesButton = () => {
-  const op = useRef<SMPopUpRef>(null);
-  const closeOverlay = () => op.current?.hide();
+	const op = useRef<SMPopUpRef>(null);
+	const closeOverlay = () => op.current?.hide();
 
-  return (
-    <SMPopUp
-      buttonClassName="sm-w-4rem icon-green"
-      buttonLabel="M3U"
-      contentWidthSize="4"
-      header={<M3UFileCreateDialog onUploadComplete={closeOverlay} />}
-      icon="pi-pencil"
-      iconFilled
-      info=""
-      modal
-      onOkClick={undefined}
-      placement="bottom-end"
-      ref={op}
-      title="M3U FILES"
-    >
-      <M3UFilesDataSelector />
-    </SMPopUp>
-  );
+	return (
+		<SMPopUp
+			buttonClassName="sm-w-6rem icon-green"
+			buttonLabel="M3U Files"
+			contentWidthSize="4"
+			header={<M3UFileCreateDialog onUploadComplete={closeOverlay} />}
+			icon="pi-pencil"
+			iconFilled
+			info=""
+			modal
+			onOkClick={undefined}
+			placement="bottom-end"
+			ref={op}
+			title="M3U FILES"
+		>
+			<M3UFilesDataSelector />
+		</SMPopUp>
+	);
 };
 
-M3UFilesButton.displayName = 'M3UFilesButton';
+M3UFilesButton.displayName = "M3UFilesButton";
 
 export interface M3UFilesEditorProperties {}
 
