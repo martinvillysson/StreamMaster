15a9af0d074470cb4f7126831ec71189349a234d
Carl Reid <kontakt@carlreid.dk>
2025-02-25 16:46:14 +0100

    fix: Shellcheck related suggestions and fixes

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -49,23 +49,19 @@ safe_chown() {
 
 # Function to check if a specific MigrationId exists in the __EFMigrationsHistory table
 check_migration_exists() {
-    local migration_id=$1
+    local migration_id="$1"
+    local table_exists
+    local exists
 
-    # Check if the __EFMigrationsHistory table exists
-    local table_exists=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -tAc "SELECT to_regclass('public.\"__EFMigrationsHistory\"');")
+    table_exists=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -tAc "SELECT to_regclass('public.\"__EFMigrationsHistory\"');")
 
     if [ "$table_exists" = "public.__EFMigrationsHistory" ]; then
-        # Table exists, check if the migration_id exists
-        local exists=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -tAc "SELECT 1 FROM public.\"__EFMigrationsHistory\" WHERE \"MigrationId\" = '$migration_id';")
+        exists=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -tAc "SELECT 1 FROM public.\"__EFMigrationsHistory\" WHERE \"MigrationId\" = '$migration_id';")
         if [ "$exists" = "1" ]; then
             return 0 # MigrationId exists
-        else
-            return 1 # MigrationId does not exist
         fi
-    else
-        # Table does not exist
-        return 1
     fi
+    return 1
 }
 
 # Perform the database migration check and update
@@ -77,18 +73,16 @@ perform_migration_update() {
     if check_migration_exists "$migration_id_to_check"; then
         echo "MigrationId $migration_id_to_check exists. Performing DELETE and INSERT operations."
 
-        psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c 'DELETE FROM public."__EFMigrationsHistory";'
-        psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "INSERT INTO public.\"__EFMigrationsHistory\"(\"MigrationId\", \"ProductVersion\") VALUES ('$new_migration_id', '$product_version');"
+        psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -c 'DELETE FROM public."__EFMigrationsHistory";'
+        psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -c "INSERT INTO public.\"__EFMigrationsHistory\"(\"MigrationId\", \"ProductVersion\") VALUES ('$new_migration_id', '$product_version');"
 
         echo "MigrationId updated to $new_migration_id with ProductVersion $product_version."
-    # else
-    #     echo "MigrationId $migration_id_to_check does not exist. No changes made."
     fi
 }
 
 moveFilesAndDeleteDir() {
-    local source_dir=$1
-    local destination_dir=$2
+    local source_dir="$1"
+    local destination_dir="$2"
 
     # Check if the source directory exists
     if [ -d "$source_dir" ]; then
@@ -101,22 +95,18 @@ moveFilesAndDeleteDir() {
         local files=("$source_dir"/*)
         if [ -e "${files[0]}" ]; then
             # Move files from source to destination
-            mv "$source_dir"/* "$destination_dir"/ 2>/dev/null
-
-            # Check if move operation was successful
-            if [ $? -eq 0 ]; then
+            if mv "$source_dir"/* "$destination_dir"/ 2>/dev/null; then
                 echo "Files moved successfully from $source_dir to $destination_dir."
             else
                 echo "Failed to move files from $source_dir to $destination_dir."
-                return # Exit the function if moving files failed
+                return
             fi
         else
             echo "No files to move from $source_dir."
         fi
 
         # Remove the source directory
-        rmdir "$source_dir" 2>/dev/null
-        if [ $? -eq 0 ]; then
+        if rmdir "$source_dir" 2>/dev/null; then
             echo "Source directory $source_dir removed."
         else
             echo "Failed to remove source directory $source_dir. It might not be empty."
@@ -166,16 +156,13 @@ rename_directory() {
 
     # Check for case sensitivity and existence of the destination directory
     if [ "$src" = "$dest" ]; then
-        #echo "Source and destination are the same in a case-insensitive filesystem."
         return 1
     elif [ -d "$dest" ]; then
-        #echo "Destination directory already exists: $dest"
         return 1
     fi
 
     # Perform the rename
-    mv "$src" "$dest"
-    if [ $? -eq 0 ]; then
+    if mv "$src" "$dest"; then
         echo "Directory renamed successfully from $src to $dest"
     else
         echo "Failed to rename directory from $src to $dest"
@@ -188,18 +175,16 @@ wait_for_postgres() {
     local port="$2"
     local max_attempts="$3"
     local wait_interval="$4"
-
     local attempt=0
 
-    while [ $attempt -lt $max_attempts ]; do
+    while [ $attempt -lt "$max_attempts" ]; do
         if pg_isready -h "$host" -p "$port" >/dev/null 2>&1; then
             echo "PostgreSQL is ready on $host:$port"
             return 0
-        else
-            attempt=$((attempt + 1))
-            echo "Attempt $attempt: PostgreSQL is not yet ready on $host:$port. Retrying in $wait_interval seconds..."
-            sleep "$wait_interval"
         fi
+        attempt=$((attempt + 1))
+        echo "Attempt $attempt: PostgreSQL is not yet ready on $host:$port. Retrying in $wait_interval seconds..."
+        sleep "$wait_interval"
     done
 
     echo "Error: PostgreSQL on $host:$port is not ready after $max_attempts attempts."
@@ -208,10 +193,10 @@ wait_for_postgres() {
 
 # Check if PUID or PGID is set to a non-root value
 if [ "$PUID" -ne 0 ]; then
-    if getent passwd $PUID >/dev/null 2>&1; then
-        user_name=$(getent passwd $PUID | cut -d: -f1)
+    if getent passwd "$PUID" >/dev/null 2>&1; then
+        user_name=$(getent passwd "$PUID" | cut -d: -f1)
     else
-        useradd --uid $PUID -K UID_MIN=10 --comment "nonRootUser" --shell /bin/bash nonRootUser || {
+        useradd --uid "$PUID" -K UID_MIN=10 --comment "nonRootUser" --shell /bin/bash nonRootUser || {
             echo "Failed to create user with PUID: $PUID"
             exit 1
         }
@@ -219,10 +204,10 @@ if [ "$PUID" -ne 0 ]; then
 fi
 
 if [ "$PGID" -ne 0 ]; then
-    if getent group $PGID >/dev/null 2>&1; then
-        group_name=$(getent group $PGID | cut -d: -f1)
+    if getent group "$PGID" >/dev/null 2>&1; then
+        group_name=$(getent group "$PGID" | cut -d: -f1)
     else
-        addgroup --gid $PGID --force-badname "nonRootGroup" || {
+        addgroup --gid "$PGID" --force-badname "nonRootGroup" || {
             echo "Failed to create group with PGID: $PGID"
             exit 1
         }
@@ -245,10 +230,18 @@ if [ "$PUID" -ne 0 ] || [ "$PGID" -ne 0 ]; then
     safe_chown "${PUID:-0}:${PGID:-0}" "/app"
 
     # Handle /config directory permissions
+    # Create a temporary script for find -exec
+    temp_script=$(mktemp)
+    echo '#!/bin/sh' > "$temp_script"
+    echo "chown -R ${PUID:-0}:${PGID:-0} \"\$1\"" >> "$temp_script"
+    chmod +x "$temp_script"
+
     find /config -mindepth 1 -maxdepth 1 -type d \
         -not -path '/config/tv-logos' \
         -not -path '/config/DB' \
-        -exec safe_chown -R "${PUID:-0}:${PGID:-0}" {} \;
+        -exec "$temp_script" {} \;
+
+    rm "$temp_script"
 
     # Handle special directories
     safe_chown "${PUID:-0}:${PGID:-0}" '/config/tv-logos'
@@ -274,10 +267,10 @@ echo "  Default SSL Port: $DEFAULT_SSL_PORT"
 echo "PGADMIN:"
 echo "  Platform Type: $PGADMIN_PLATFORM_TYPE"
 echo "  Setup Email: $PGADMIN_SETUP_EMAIL"
-echo "  Setup Password: ********" #$PGADMIN_SETUP_PASSWORD"
+echo "  Setup Password: ********"
 echo "POSTGRES:"
 echo "  User: $POSTGRES_USER"
-echo "  Password: ********" #$POSTGRES_PASSWORD"
+echo "  Password: ********"
 echo "  DB Name: $POSTGRES_DB"
 echo "  Host: $POSTGRES_HOST"
 echo "  Is DB Local: $POSTGRES_ISLOCAL"
@@ -291,38 +284,27 @@ echo "  UID: $(id -u postgres) GID: $(id -g postgres)"
 if [ "$POSTGRES_ISLOCAL" -eq 1 ]; then
     # Start the database
     /usr/local/bin/docker-entrypoint.sh postgres &
-
 fi
 
-wait_for_postgres $POSTGRES_HOST "5432" 20 5
-if [ $? -eq 0 ]; then
-    # PostgreSQL is ready, you can proceed with your tasks
+if wait_for_postgres "$POSTGRES_HOST" "5432" 20 5; then
     echo "Postgres is up"
 
-    # Check if any file is ready for restore and run restore.sh if so
     if check_files_ready_for_restore; then
-        # Print a warning message about the restoration process
         echo "WARNING: You are about to restore the database. This operation cannot be undone."
         echo "The restoration process will begin in 10 seconds. Press Ctrl+C to cancel."
-
-        # Pause for 10 seconds to give the user a chance to cancel
         sleep 10
-
         echo "Initiating restoration process..."
         /usr/local/bin/restore.sh
     else
         echo "No files ready for restoration."
-        # Perform migration update
         perform_migration_update
     fi
-
 else
-    # PostgreSQL is not ready after max_attempts, handle the error
     echo "Error: PostgreSQL is not ready."
     exit 1
 fi
 
-moveFilesAndDeleteDir /config/DB/Backup $BACKUP_DIR
+moveFilesAndDeleteDir /config/DB/Backup "$BACKUP_DIR"
 
 safe_chown "${PUID:-0}:${PGID:-0}" "$BACKUP_DIR"
 safe_chown "${PUID:-0}:${PGID:-0}" "$RESTORE_DIR"
@@ -339,13 +321,13 @@ fi
 # Execute the main application as the specified user
 if [ "$PUID" -ne 0 ] && [ "$PGID" -ne 0 ]; then
     echo "Running as $user_name:$group_name"
-    exec gosu $user_name:$group_name "$@"
+    exec gosu "$user_name:$group_name" "$@"
 elif [ "$PUID" -ne 0 ]; then
     echo "Running as $user_name"
-    exec gosu $user_name "$@"
+    exec gosu "$user_name" "$@"
 elif [ "$PGID" -ne 0 ]; then
     echo "Running as :$group_name"
-    exec gosu :$group_name "$@"
+    exec gosu ":$group_name" "$@"
 else
     echo "Running as root"
     exec "$@"
